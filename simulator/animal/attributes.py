7dd61615421fc5a1e68e64edd7c865e9c4d593b7
Rocky <rockysn.96@gmail.com>
2023-03-10 15:22:47 -0500

    Defining starting parameters of each animal

---
+++ simulator/animal/attributes.py
@@ -0,0 +1,75 @@
+import numpy.random as npr
+import scipy.stats as sp
+import yaml
+from timeit import default_timer as timer
+import itertools
+from dataclasses import dataclass
+
+
+@dataclass
+class StartingAttr:
+    stream = open("../config.yaml", "r")
+    data = yaml.safe_load(stream)
+    id_iter = itertools.count()
+
+    def __post_init__(self):
+        self.id = next(self.id_iter)
+
+    @classmethod
+    def weight(cls) -> int:
+        starting_weight = cls.data["weight"][0]
+        ending_weight = cls.data["weight"][1]
+        return int(npr.uniform(starting_weight, ending_weight))
+
+    @classmethod
+    def sex_male(cls) -> bool:
+        if npr.binomial(1, 0.5) == 0:
+            return True
+        else:
+            return False
+
+    # These functions are only for initializing our
+    # simulation based on yaml config
+
+
+class InitialPopulation(StartingAttr):
+    @classmethod
+    def starting_age(cls) -> int:
+        starting_age = cls.data["age"][0]
+        ending_age = cls.data["age"][1]
+        return int(npr.uniform(starting_age, ending_age))
+
+    @classmethod
+    def starting_alive(cls) -> bool:
+        return True
+
+    @classmethod
+    def starting_gene_edit(cls) -> bool:
+        if npr.binomial(1, 0.1) == 0:
+            return True
+        else:
+            return False
+
+    # These functions are for all newborns
+    # based on yaml configs
+
+
+class NewBorn(StartingAttr):
+    @classmethod
+    def age(cls):
+        return 0
+
+    @classmethod
+    def gene_edit(cls):
+        if npr.binomial(1, cls.data["gene_edit_rate"]) == 1:
+            return True
+        else:
+            return False
+
+
+if __name__ == "__main__":
+    start = timer()
+    for i in range(10):
+        print(NewBorn.gene_edit())
+    end = timer()
+    print(end - start)
--- simulator/tests/test_random_attr.py
+++ simulator/tests/test_random_attr.py
@@ -1,20 +1,42 @@
-from simulator.animal import random_attr
+from simulator.animal import attributes
 import unittest
 import yaml
 
-print(random_attr.RandomAttr.age())
+stream = open("../config.yaml", "r")
+data = yaml.safe_load(stream)
 
 
-class TestRandomAttr(unittest.TestCase):
-    stream = open("../config.yaml", "r")
-    data = yaml.safe_load(stream)
+class TestStartingAttr(unittest.TestCase):
+    def test_weight(self):
+        self.assertIs(type(attributes.InitialPopulation.weight()), int)
+        self.assertLessEqual(attributes.InitialPopulation.weight(), data["weight"][1])
+        self.assertGreaterEqual(attributes.InitialPopulation.weight(), data["weight"][0])
 
+    def test_sex_male(self):
+        self.assertIs(type(attributes.InitialPopulation.sex_male()), bool)
+
+
+class TestInitialPopulation(unittest.TestCase):
+
+    def test_starting_age(self):
+        self.assertIs(type(attributes.InitialPopulation.starting_age()), int)
+        self.assertLessEqual(attributes.InitialPopulation.starting_age(), data["age"][1])
+        self.assertGreaterEqual(attributes.InitialPopulation.starting_age(), data["age"][0])
+
+    def test_starting_alive(self):
+        self.assertIs(type(attributes.InitialPopulation.starting_alive()), bool)
+
+    def test_starting_gene_edit(self):
+        self.assertIs(type(attributes.InitialPopulation.starting_gene_edit()), bool)
+
+
+class TestNewBorn(unittest.TestCase):
     def test_age(self):
-        self.assertIs(type(random_attr.RandomAttr.age()), int)
-        self.assertLessEqual(random_attr.RandomAttr.age(), self.data["age"][1])
-        self.assertGreaterEqual(random_attr.RandomAttr.age(), self.data["age"][0])
+        self.assertIs(attributes.NewBorn.age() == 0)
+
+    def test_gene_edit(self):
+        self.assertIs(type(attributes.NewBorn.gene_edit()), bool)
 
 
-#
 if __name__ == '__main__':
     unittest.main()
